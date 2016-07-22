01937702078a524b66793633f18b54f60453ce4c
pgbarletta <pbarletta@gmail.com>
2016-07-22 10:53:34 -0300

    reorganize files

--- sync.sh
+++ sync.sh
@@ -4,8 +4,8 @@ backup_list="src top_files README.md journal graphs sistemas-de-partida"
 for folder in $backup_list
 do
 	echo "------------ syncronizo $folder ------------"
-	rsync -rvzh --update -e 'ssh -p4222' $folder patricio@200.5.224.86:/home/patricio/hemo
-	ssh -p4222 patricio@200.5.224.86 rsync -rvzh --update -e 'ssh' hemo/$folder pato@10.6.0.122:/home/pato/hemo/
+	rsync -rvzh --update -e 'ssh -p4222' $folder patricio@207.248.75.21:/home/patricio/hemo
+	ssh -p4222 patricio@207.248.75.21 rsync -rvzh --update -e 'ssh' hemo/$folder pato@10.6.0.122:/home/pato/hemo/
 	echo "------------ listo con $folder ------------"		
 done
 exit 0
